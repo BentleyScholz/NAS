1d2130c9d01ef3cbc5235bb2901e24d9c83a7aa6
BentleyScholz <scholzbentley@gmail.com>
2023-01-16 19:06:52 -0700

    Got the query to work

--- connection_test/connector.py
+++ connection_test/connector.py
@@ -48,13 +48,29 @@ if __name__ == "__main__":
     cur.execute(query)
 
     conn.select_db(new_db=database_name)
-    print(f'Connected to {database_name}')
+    print(f'Connected to {conn.database}')
 
     ##################################################
     # erase and reacreate test table
     ##################################################
 
     ### Read in data so we know what fields we need
+    # make sure char array size matches with the string length in datagen.py
+
+    table_schema = '''
+    (Date DATE, Int_Field INT(8) UNSIGNED, Norm_Field_1 FLOAT, Norm_Field_2 FLOAT, Norm_Field_3 FLOAT, Norm_Field_4 FLOAT, Char_Field_1 CHAR(30), Char_Field_2 CHAR(30), Char_Field_3 CHAR(30))
+    '''
+
+    query = f'''
+    CREATE OR REPLACE TABLE {database_table} {table_schema};
+    '''
+
+    print(f'Executing query {query}')
+    cur.execute(query)
+
+    ##################################################
+    # load data and write to db
+    ##################################################
 
     # get filenames in data directory
     files_in_dir = os.listdir(data_dir)
@@ -68,29 +84,34 @@ if __name__ == "__main__":
         quoting=3
     )
 
-    # Helper function for converting pandas dtypes to sql dtypes
-    # def convert_dtypes(dtype:str):
-    #     if 
-
-    schema = dict(
-        zip(
-            df.dtypes.index,
-            df.dtypes.values
-        )
-    )
-
-    print(schema)
-
-    query = f'''
-    CREATE OR REPLACE {database_table};
+    table_fields = '''
+    (Date, Int_Field, Norm_Field_1, Norm_Field_2, Norm_Field_3, Norm_Field_4, Char_Field_1. Char_Field_2, Char_Field_3)
     '''
 
-    # print(f'Executing query {query}')
-    # cur.execute(query)
+    query = f'''
+    INSERT INTO {database_table} VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);
+    '''
 
+    values_tuple = [tuple(x) for x in df.values]
+
+    cur.executemany(
+        query,
+        values_tuple[:1000]
+    )
+    
     ##################################################
-    # load data and write to db
+    # query data to make sure the tables arent empty
     ##################################################
 
-    # Helper function for formating queries
+    print('Found the following data after inserting:')
 
+    cur.execute(
+        f'SELECT * FROM {database_table}'
+    )
+    limit = 10
+    token = 0
+    for x in cur:
+        if token >= limit:
+            break
+        else:
+            print(x)
